0f251f1a3ddbca7b4c64518592450590a59b6ba4
Udeani4 <udeaniizu04@gmail.com>
2026-09-18 17:20:48 +0100

    macro data sent to mongodb

--- push_data.py
+++ push_data.py
@@ -102,6 +102,7 @@ class NGXStockDataExtract:
         for ticker in list_of_tickers[:10]:
             self.get_stock_data_from_ngx_pulse(ticker,ngx_api_key)
 
+
     def csv_to_json_converter(self, file_path):
         """
         Reads a CSV file and converts it into a list of JSON-like records.
@@ -155,26 +156,34 @@ class NGXStockDataExtract:
 
 
 if __name__ == '__main__':
-    DATABASE = "NGX_Stock_ME_Database"
-    Collection = "stock_data"
+    # DATABASE = "NGX_Stock_ME_Database"
+    # Collection = "stock_data"
 
-    ngxstockobj = NGXStockDataExtract()
-    stock_files = os.listdir('stock_data')
+    # ngxstockobj = NGXStockDataExtract()
+    # stock_files = os.listdir('stock_data')
 
-    for data in stock_files:
+    # for data in stock_files:
 
-        if data.endswith(".csv"):
+    #     if data.endswith(".csv"):
 
-            FILE_PATH = f"stock_data/{data}"
+    #         FILE_PATH = f"stock_data/{data}"
 
-            records = ngxstockobj.csv_to_json_converter(
-                file_path=FILE_PATH
-            )
+    #         records = ngxstockobj.csv_to_json_converter(
+    #             file_path=FILE_PATH
+    #         )
 
-            no_of_records = ngxstockobj.insert_data_to_mongodb(
-                records,
-                DATABASE,
-                Collection
-            )
+    #         no_of_records = ngxstockobj.insert_data_to_mongodb(
+    #             records,
+    #             DATABASE,
+    #             Collection
+    #         )
 
-            print(f"{data} inserted to mongodb: {no_of_records} records")
+    #         print(f"{data} inserted to mongodb: {no_of_records} records")
+
+    FILE_PATH="./macroeconomic_data/populated_cleaned_macro_data_adjusted2"
+    DATABASE="NGX_Stock_ME_Database"
+    Collection="macro_economic_data"
+    ngxstockdataobj=NGXStockDataExtract()
+    records=ngxstockdataobj.csv_to_json_converter(file_path=FILE_PATH)
+    no_of_records=ngxstockdataobj.insert_data_to_mongodb(records,DATABASE,Collection)
+    print(no_of_records)
